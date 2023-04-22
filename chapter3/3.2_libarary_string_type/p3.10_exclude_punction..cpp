def858a0c5ffd52df716e07654138d2ac8b6d939
CanGroves <172975487@qq.com>
2023-04-22 20:02:34 +0800

    exer 3.10 exclude punction by two pointers

---
+++ chapter3/3.2_libarary_string_type/p3.10_exclude_punction..cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::getline;
+using std::string;
+int main()
+{
+    string input;
+    cin >> input;
+    decltype(input.size()) i = 0, j = 0;
+    for ( ; i < input.size() && j < input.size(); ++i, ++j) {
+        while (j < input.size() && ispunct(input[j])) {
+            ++j;
+        }
+        if (j < input.size()) {
+            input[i] = input[j];
+        } else {
+            --i;
+        }
+    }
+    input.resize(i);
+    
+    cout << input << endl;
+}
