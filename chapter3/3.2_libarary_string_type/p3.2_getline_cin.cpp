ab0b9907e8c7e5c26002788bd7d086c249cab820
CanGroves <172975487@qq.com>
2023-04-22 16:57:05 +0800

    exer 3.2

---
+++ chapter3/3.2_libarary_string_type/p3.2_getline_cin.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::getline;
+using std::string;
+int main()
+{
+    string input;
+    getline(cin, input);
+    cout << input << endl;
+    string input1;
+    cin >> input1;
+    cout << input1 << endl;
+}
